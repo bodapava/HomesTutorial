437794b299b54625f69715405c0243d7c48116f7
Priyanka <priyanka.bvv18@gmail.com>
2023-06-11 17:30:19 -0400

    Final changes done!!

--- HomesApp/src/app/components/houses/houses.component.ts
+++ HomesApp/src/app/components/houses/houses.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { DocumentData } from 'firebase/firestore';
 import { HouseService } from 'src/app/houses.service';
 import { Houses } from 'src/app/models/houses';
@@ -8,20 +8,40 @@ import { Houses } from 'src/app/models/houses';
   templateUrl: './houses.component.html',
   styleUrls: ['./houses.component.css'],
 })
-export class HousesComponent {
+export class HousesComponent implements OnChanges {
   houseList: any;
+  filteredList: any;
+  @Input() location: any;
   constructor(private houseService: HouseService) {
+    console.log('houses', this.location);
     this.getListHouses();
+    // this.filteredListVal();
+  }
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('houses', changes);
+    this.filteredListVal(this.location);
   }
 
   getListHouses() {
     this.houseService.getHouses().then((data) => {
       console.log(data);
-      this.houseList = data;
-
+      this.houseList = this.filteredList = data;
       console.log('Components data ', this.houseList);
     });
-
     return this.houseList;
+  }
+
+  filteredListVal(value?: String) {
+    if (this.houseList?.length != 0) {
+      this.filteredList = value
+        ? this.houseList?.filter((house: any) =>
+            house.location.toLowerCase().startsWith(value?.toLowerCase())
+          )
+        : this.houseList;
+    }
+
+    console.log('filter', this.filteredList);
+
+    return this.filteredList;
   }
 }
--- HomesApp/src/app/components/nav-bar/nav-bar.component.ts
+++ HomesApp/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent {
-
+  @Output() searchEvent = new EventEmitter();
+  sendValues(e: any) {
+    console.log('navbar', e.target.value);
+    this.searchEvent.emit(e.target.value);
+  }
 }
