d5166e8104edc065fa593587500d077b5bdb21de
Priyanka <priyanka.bvv18@gmail.com>
2023-06-11 16:16:35 -0400

    WORKING UI

---
+++ HomesApp/src/app/environment.ts
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAD4KQwVtnlMViNl447C7IRLwPexw9GrDE',
+  authDomain: 'homeapp-bcd7e.firebaseapp.com',
+  projectId: 'homeapp-bcd7e',
+  storageBucket: 'homeapp-bcd7e.appspot.com',
+  messagingSenderId: '456950251128',
+  appId: '1:456950251128:web:de2e4fa1f5a348f5e9f78d',
+};
+
+// Initialize Firebase
